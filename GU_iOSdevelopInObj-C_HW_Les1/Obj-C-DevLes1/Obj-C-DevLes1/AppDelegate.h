f1fd338c99f10552ccdaaa7f6ae9346af96a56fd
romankolossov <rkolossov@gmail.com>
2021-01-16 21:36:36 +0300

    Lesson1 home work started

---
+++ GU_iOSdevelopInObj-C_HW_Les1/Obj-C-DevLes1/Obj-C-DevLes1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Obj-C-DevLes1
+//
+//  Created by Roman Kolosov on 16.01.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GU_iOSdevelopInObj-C_HW_Les1/Obj-C-DevLes1/Obj-C-DevLes1/MainViewController.m
@@ -0,0 +1,23 @@
+//
+//  MainViewController.m
+//  Obj-C-DevLes1
+//
+//  Created by Roman Kolosov on 16.01.2021.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+@end
